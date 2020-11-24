307fb476fbeee3a0a8faa182867884bba9359323
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2020-11-24 20:08:57 -0300

    Add GetProp to key type

--- assets/getProp.go
+++ assets/getProp.go
@@ -5,3 +5,9 @@ func (a Asset) GetProp(propTag string) interface{} {
 	val, _ := a[propTag]
 	return val
 }
+
+// GetProp returns the prop value. It returns nil if it doesn't exist.
+func (k Key) GetProp(propTag string) interface{} {
+	val, _ := k[propTag]
+	return val
+}
